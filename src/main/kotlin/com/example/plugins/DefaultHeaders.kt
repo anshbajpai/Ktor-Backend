86813611042f7b15a83bf983123e211a2fa42710
Ansh <abaj0005>
2021-12-15 14:32:54 +0530

    Initial commit

---
+++ src/main/kotlin/com/example/plugins/DefaultHeaders.kt
@@ -0,0 +1,14 @@
+package com.example.plugins
+
+import io.ktor.application.*
+import io.ktor.features.*
+import io.ktor.http.*
+import java.time.Duration
+
+fun Application.configureDefaultHeader() {
+    install(DefaultHeaders) {
+        val oneYearInSeconds = Duration.ofDays(365).seconds
+        header(HttpHeaders.CacheControl,
+            "public, max-age=$oneYearInSeconds, immutable")
+    }
+}
